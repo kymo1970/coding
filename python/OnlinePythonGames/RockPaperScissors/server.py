7430fef2025b722f0ab233eba71e262c5e765fdc
James Eyrich <kymo1970@gmail.com>
2024-01-20 10:51:55 -0700

    Updates

--- python/OnlinePythonGames/RockPaperScissors/server.py
+++ python/OnlinePythonGames/RockPaperScissors/server.py
@@ -1,51 +1,78 @@
+# import socket
+# from _thread import *
+# import sys
+
+# server = "192.168.12.192"
+# port = 5555
+
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# try:
+#     sock.bind((server, port))
+# except socket.error as e:
+#     str(e)
+
+# sock.listen(2)
+# print("Waiting for connection, Server has been started.")        
+
+
+# pos = [(0, 0), (400, 400)]
+# def threadedClient(conn):
+#     conn.send(str.encode("Connected"))
+#     reply = ""
+#     while True:
+#         try:
+#             data = conn.recv(2048)
+#             reply = data.decode("utf-8")
+            
+#             if not data:
+#                 print("Disconnected")
+#                 break
+#             else:
+#                 print("Received: ", reply)
+#                 print("Sending: ", reply)
+                
+#             conn.sendall(str.encode(reply))
+#         except:
+#             break        
+    
+#     print("Lost Connection!")
+#     conn.close()        
+
+# currentPlayer = 0
+# while True:
+#     conn, addr = sock.accept()
+#     print("Connected to: ", addr)
+    
+#     start_new_thread(threadedClient, (conn,))
+    
+#     currentPlayer += 1
+    
 import socket
 from _thread import *
 import sys
 
-server = "192.168.12.192"
+server = ""
 port = 5555
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    sock.bind((server, port))
+    s.bind((server, port))
 except socket.error as e:
     str(e)
-
-sock.listen(2)
-print("Waiting for connection, Server has been started.")        
-
-
-pos = [(0, 0), (400, 400)]
-def threadedClient(conn):
-    conn.send(str.encode("Connected"))
-    reply = ""
-    while True:
-        try:
-            data = conn.recv(2048)
-            reply = data.decode("utf-8")
-            
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                print("Received: ", reply)
-                print("Sending: ", reply)
-                
-            conn.sendall(str.encode(reply))
-        except:
-            break        
     
-    print("Lost Connection!")
-    conn.close()        
+s.listen(2)
+print("Waiting on connection, server has been started!")
 
-currentPlayer = 0
+
+def threadedClient(conn):
+    pass
+
+
 while True:
-    conn, addr = sock.accept()
+    conn, addr = s.accept()
     print("Connected to: ", addr)
     
-    start_new_thread(threadedClient, (conn,))
-    
-    currentPlayer += 1
-    
+    start_new_thread(threadedClient, (conn, 1))
     
